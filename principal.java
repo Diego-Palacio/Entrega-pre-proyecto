cf2a55bbf200c94909b6508a99eab078a60d64f1
diego-palacio <diego_palacio2015@hotmail.com>
2025-05-18 23:57:22 -0300

    funcionalidad crear, listar articulos

--- principal.java
+++ principal.java
@@ -20,13 +20,33 @@ public class principal {
             scan.nextLine(); // Limpiar buffer
 
             switch (opcion) {
-                case 1 -> System.out.println("crearArticulo()");
-                case 2 -> System.out.println("listarArticulos()"); 
+                case 1 -> crearArticulo();
+                case 2 -> listarArticulos(); 
                 case 3 -> System.out.println("modificarArticulo()");
                 case 4 -> System.out.println("eliminarArticulo()");
                 case 5 -> System.out.println("Saliendo...");
                 default -> System.out.println("Opción inválida");
             }
         } while (opcion != 5);
-    }
+        }
+
+        
+        public static void crearArticulo(){
+            System.out.println("Ingrese nombre del articulo: ");
+            String articulo = scan.nextLine();
+            System.out.println("Ingrese nombre del precio: ");
+            int precio = scan.nextInt();
+            productos.add(new producto(articulo, precio, 0));
+            System.out.println("Articulo "+articulo+ " agregado");
+
+            System.out.println(productos);
+
+        }
+
+
+        public static void listarArticulos(){
+            for(producto prod: productos){
+                System.out.println(prod.toString());
+            }
+        }
 }
