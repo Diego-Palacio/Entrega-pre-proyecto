38ef25c4475040c11396efc325a7e8e054ee467c
diego-palacio <diego_palacio2015@hotmail.com>
2025-05-18 21:56:57 -0300

    primer commit

---
+++ main.java
@@ -0,0 +1,7 @@
+public class main {
+    public static void main(String[] args) {
+        producto producto1= new producto("Coca cola", 4000, 10);
+
+        System.out.println(producto1.toString());
+    }
+}
