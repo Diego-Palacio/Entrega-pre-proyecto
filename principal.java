b19855f415c032944ec2cd90db73b41efb129719
diego-palacio <diego_palacio2015@hotmail.com>
2025-05-19 23:02:56 -0300

    funcionalidad modificarArticulo

--- principal.java
+++ principal.java
@@ -22,7 +22,7 @@ public class principal {
             switch (opcion) {
                 case 1 -> crearArticulo();
                 case 2 -> listarArticulos(); 
-                case 3 -> System.out.println("modificarArticulo()");
+                case 3 -> modificarArticulo();
                 case 4 -> System.out.println("eliminarArticulo()");
                 case 5 -> System.out.println("Saliendo...");
                 default -> System.out.println("Opción inválida");
@@ -34,19 +34,52 @@ public class principal {
         public static void crearArticulo(){
             System.out.println("Ingrese nombre del articulo: ");
             String articulo = scan.nextLine();
-            System.out.println("Ingrese nombre del precio: ");
+            System.out.println("Ingrese el precio: ");
             int precio = scan.nextInt();
-            productos.add(new producto(articulo, precio, 0));
-            System.out.println("Articulo "+articulo+ " agregado");
 
-            System.out.println(productos);
+            if(articulo!=""){
+                producto productoNuevo= new producto(articulo, precio, 0);
+                productos.add(productoNuevo);
+                System.out.println("Articulo "+articulo+ " agregado");
+                System.out.println(productos);
+            }
+            else{
+                System.out.println("No puede ingresar articulo sin nombre");
+            }
+            
 
         }
 
 
         public static void listarArticulos(){
-            for(producto prod: productos){
+            if (productos.size()!=0){
+                 for(producto prod: productos){
                 System.out.println(prod.toString());
+                }     
+            }
+            else{
+                System.out.println("No hay ningun producto agregado en la lista.2");
+            }
+           
+        }
+
+        public static void modificarArticulo(){
+            System.out.println("Ingrese el nombre del articulo a modificar");
+            String nombreProducto= scan.nextLine();
+
+            for(producto p: productos){
+            System.out.println("Ingrese el nuevo nombre al producto: ");
+            String nuevoNombreProducto= scan.nextLine();
+
+            if(p.nombre==(nombreProducto)){
+                System.out.println("producto "+ nombreProducto +" existente");
+                p.setNombre(nuevoNombreProducto);
+                System.out.println("nombre de Producto "+nombreProducto+ "cambiado a "+ nuevoNombreProducto);
             }
         }
+            System.out.println("No existe producto "+ nombreProducto);
+        
+        }
+
+
 }
